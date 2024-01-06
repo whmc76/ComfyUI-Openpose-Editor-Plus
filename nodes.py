87dbfdcac26d05a18deb1d6f4676471676ec9311
whmc76 <286878701@qq.com>
2024-01-07 01:57:05 +0800

    add ref button

--- nodes.py
+++ nodes.py
@@ -1,27 +1,54 @@
-import os.path
+from PIL import Image
+import os
 import folder_paths
-from nodes import LoadImage
+import hashlib
+import torch
+import numpy as np
 
+class openPoseEditorPlus:
+  @classmethod
+  def INPUT_TYPES(self):
+    temp_dir = folder_paths.get_temp_directory()
 
-class OpenPoseEditorPlus:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
-                    {"image": ("STRING", { "default": "" })},
-                }
+    if not os.path.isdir(temp_dir):
+      os.makedirs(temp_dir)
 
-    RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "load_image"
+    temp_dir = folder_paths.get_temp_directory()
 
-    CATEGORY = "image"
+    return {"required":
+              {"image": (sorted(os.listdir(temp_dir)),)},
+            }
 
-    def load_image(self, image):
-        image, mask = LoadImage.load_image(self, image)
-        return (image,)
+  RETURN_TYPES = ("IMAGE",)
+  FUNCTION = "output_pose"
+
+  CATEGORY = "image"
+
+  def output_pose(self, image):
+    image_path = os.path.join(folder_paths.get_temp_directory(), image)
+    # print(f"Create: {image_path}")
+
+    i = Image.open(image_path)
+    image = i.convert("RGB")
+    image = np.array(image).astype(np.float32) / 255.0
+    image = torch.from_numpy(image)[None,]
+
+    return (image,)
+
+  @classmethod
+  def IS_CHANGED(self, image):
+    image_path = os.path.join(
+      folder_paths.get_temp_directory(), image)
+    # print(f'Change: {image_path}')
+
+    m = hashlib.sha256()
+    with open(image_path, 'rb') as f:
+      m.update(f.read())
+    return m.digest().hex()
 
 
 NODE_CLASS_MAPPINGS = {
-    "CDL.OpenPoseEditorPlus": OpenPoseEditorPlus
+    "CDL.OpenPoseEditorPlus": openPoseEditorPlus
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
